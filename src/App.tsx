1978aa720024371a241b151ba355a0fa08279f56
Hans-Christian Otto <c.otto@suora.com>
2022-04-26 23:07:23 +0100

    Extracted HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,11 @@ import React from 'react';
 
 import './App.css';
 
-import useTimeEntries from "./hooks/useTimeEntries";
-import {TimeEntryList} from "./components/TimeEntryList";
-import {TimeEntryForm} from "./components/TimeEntryForm";
-import ProjectList from "./components/ProjectList";
-import useProjects from "./hooks/useProjects";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  const {timeEntries, logTime} = useTimeEntries();
-  const {projects, loading, error} = useProjects()
-
   return (
-    <div className="content">
-      <h1>Time Entries</h1>
-      <TimeEntryList timeEntries={timeEntries}/>
-      <TimeEntryForm
-        onCreateTimeEntry={(timeEntry) => {
-          logTime({...timeEntry, projectId: "workshop"});
-        }}
-      />
-      <ProjectList loading={loading} projects={projects} error={error} />
-    </div>
+    <HomePage />
   );
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {TimeEntryForm} from "../components/TimeEntryForm";
+import useTimeEntries from "../hooks/useTimeEntries";
+import useProjects from "../hooks/useProjects";
+import ProjectList from "../components/ProjectList";
+import {TimeEntryList} from "../components/TimeEntryList";
+
+function HomePage() {
+  const {timeEntries, logTime} = useTimeEntries();
+  const {projects, loading, error} = useProjects()
+
+  return (
+    <div className="content">
+      <h1>Time Entries</h1>
+      <TimeEntryList timeEntries={timeEntries}/>
+      <TimeEntryForm
+        onCreateTimeEntry={(timeEntry) => {
+          logTime({...timeEntry, projectId: "workshop"});
+        }}
+      />
+      <ProjectList loading={loading} projects={projects} error={error} />
+    </div>
+  );
+}
+
+export default HomePage;
